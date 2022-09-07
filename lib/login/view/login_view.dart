b83443ee4e131455d9d1f63b634999c183276c74
MinaSaad47 <MinaSaad-47@outlook.com>
2022-09-07 18:31:57 +0200

    [login] added scollview

--- lib/login/view/login_view.dart
+++ lib/login/view/login_view.dart
@@ -43,82 +43,90 @@ class _LoginViewState extends State<LoginView> {
           } else {
             return Padding(
               padding: const EdgeInsets.all(20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SvgPicture.asset(
-                    'assets/images/logo.svg',
-                    color: Theme.of(context).primaryColor,
-                  ),
-                  Text(
-                    AppLocalizations.of(context)!.login,
-                    style: Theme.of(context).textTheme.headline1,
-                  ),
-                  FormBuilder(
-                    key: formKey,
+              child: LayoutBuilder(
+                builder: (context, constraints) => SingleChildScrollView(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minHeight: constraints.maxHeight,
+                    ),
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        FormBuilderTextFieldWidget(
-                          context,
-                          name: AppLocalizations.of(context)!.userName,
+                        SvgPicture.asset(
+                          height: constraints.maxHeight / 3,
+                          'assets/images/logo.svg',
+                          color: Theme.of(context).primaryColor,
                         ),
-                        FormBuilderTextFieldWidget(
-                          context,
-                          name: AppLocalizations.of(context)!.password,
-                          isPassword: true,
+                        FormBuilder(
+                          key: formKey,
+                          child: Column(
+                            children: [
+                              FormBuilderTextFieldWidget(
+                                context,
+                                name: AppLocalizations.of(context)!.userName,
+                              ),
+                              FormBuilderTextFieldWidget(
+                                context,
+                                name: AppLocalizations.of(context)!.password,
+                                isPassword: true,
+                              ),
+                            ],
+                          ),
                         ),
+                        BlocConsumer<LoginCubit, LoginState>(
+                          listener: (context, state) {
+                            if (state.action == LoginAction.login &&
+                                state.status == LoginStatus.success) {
+                              context.read<LoginCubit>().getCurrentUser();
+                            } else if (state.action == LoginAction.login &&
+                                state.status == LoginStatus.failure) {
+                              Utils.toast(
+                                context,
+                                message: state.message,
+                                level: ToastLevel.error,
+                              );
+                            }
+                          },
+                          builder: (context, state) {
+                            return MaterialButton(
+                              onPressed: () async {
+                                var formState = formKey.currentState;
+                                if (formState != null && formState.validate()) {
+                                  formState.save();
+                                  var userModel = UserModel(
+                                    name: formState.value[
+                                        AppLocalizations.of(context)!.userName],
+                                    password: formState.value[
+                                        AppLocalizations.of(context)!.password],
+                                    value: 0,
+                                  );
+                                  context
+                                      .read<LoginCubit>()
+                                      .loginUser(userModel);
+                                }
+                              },
+                              color: Theme.of(context).colorScheme.secondary,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    AppLocalizations.of(context)!.login,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headline4!
+                                        .copyWith(color: Colors.white),
+                                  ),
+                                  const SizedBox(width: 5),
+                                  const Icon(Icons.login_outlined),
+                                ],
+                              ),
+                            );
+                          },
+                        )
                       ],
                     ),
                   ),
-                  BlocConsumer<LoginCubit, LoginState>(
-                    listener: (context, state) {
-                      if (state.action == LoginAction.login &&
-                          state.status == LoginStatus.success) {
-                        context.read<LoginCubit>().getCurrentUser();
-                      } else if (state.action == LoginAction.login &&
-                          state.status == LoginStatus.failure) {
-                        Utils.toast(
-                          context,
-                          message: state.message,
-                          level: ToastLevel.error,
-                        );
-                      }
-                    },
-                    builder: (context, state) {
-                      return MaterialButton(
-                        onPressed: () async {
-                          var formState = formKey.currentState;
-                          if (formState != null && formState.validate()) {
-                            formState.save();
-                            var userModel = UserModel(
-                              name: formState.value[
-                                  AppLocalizations.of(context)!.userName],
-                              password: formState.value[
-                                  AppLocalizations.of(context)!.password],
-                              value: 0,
-                            );
-                            context.read<LoginCubit>().loginUser(userModel);
-                          }
-                        },
-                        color: Theme.of(context).colorScheme.secondary,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              AppLocalizations.of(context)!.login,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headline4!
-                                  .copyWith(color: Colors.white),
-                            ),
-                            const SizedBox(width: 5),
-                            const Icon(Icons.login_outlined),
-                          ],
-                        ),
-                      );
-                    },
-                  )
-                ],
+                ),
               ),
             );
           }
